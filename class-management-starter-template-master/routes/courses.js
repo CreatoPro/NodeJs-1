3d47b030778415436472561ac8cc136c7d4729b2
Shreesat Sahu <69542882+CreatoPro@users.noreply.github.com>
2022-08-03 14:47:05 +0530

    db connected

--- class-management-starter-template-master/routes/courses.js
+++ class-management-starter-template-master/routes/courses.js
@@ -9,8 +9,28 @@ router.post("create", (req, res)=>
     console.log(name);
     if(!name) return res.status(404).json({msg: "Please enter a course name."});
 
-    let sqlCheck = `Select * from courses where slug = ?`
-    let sql = `INSERT INTO courses SET ?`
+    let sqlCheck = `Select * from courses where slug = ?`;
+    let sql = `INSERT INTO courses SET ?`;
+    const slug = sligify(name).toLowerCase();
+
+    db.query(sqlCheck, slug,(err, course) =>
+    {
+        if(course.length > 0) 
+        return res.status(400).json({msg: "Course Exits"});
+        
+
+        const data ={
+            course_name: name.toLowerCase(),
+            slug: slugify(name).toLowerCase()
+        };
+    
+        db.query(sql, data, (err, result) => {
+            if (err) return res.status(400).json({msg: "Unable to insert course"});
+
+            return res.status(200).json({data});
+        })
+    
+    })
 })
 
 
